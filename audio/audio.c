f63a5f1b6b56d3a5d325c8686a47e2dd884e483a
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-08-15 20:08:47 +0530

    audio.c
    Procedural Language: API Implementation : static void audio_run_capture (AudioState *s);

--- audio/audio.c
+++ audio/audio.c
@@ -1486,24 +1486,20 @@ static void audio_run_in (AudioState *s)
 static void audio_run_capture (AudioState *s)
 {
     CaptureVoiceOut *cap;
-
-    for (cap = s->cap_head.lh_first; cap; cap = cap->entries.le_next) {
-        int live, rpos, captured;
-        HWVoiceOut *hw = &cap->hw;
-        SWVoiceOut *sw;
-
+    int live, rpos, captured;
+    HWVoiceOut *hw = (HWVoiceOut*)&cap->hw;
+    SWVoiceOut *sw;
+    int left = ((hw->samples) - rpos);
+    int to_capture = audio_MIN (live, left);
+    struct st_sample *src;
+    struct capture_callback *cb;
+    for (cap = (CaptureVoiceOut *)s->cap_head.lh_first; cap; cap = cap->entries.le_next) {
         captured = live = audio_pcm_hw_get_live_out (hw, NULL);
         rpos = hw->rpos;
         while (live) {
-            int left = hw->samples - rpos;
-            int to_capture = audio_MIN (live, left);
-            struct st_sample *src;
-            struct capture_callback *cb;
-
-            src = hw->mix_buf + rpos;
+            src = ((struct st_sample *)hw->mix_buf) + rpos;
             hw->clip (cap->buf, src, to_capture);
             mixeng_clear (src, to_capture);
-
             for (cb = cap->cb_head.lh_first; cb; cb = cb->entries.le_next) {
                 cb->ops.capture (cb->opaque, cap->buf,
                                  to_capture << hw->info.shift);
@@ -1513,7 +1509,7 @@ static void audio_run_capture (AudioState *s)
         }
         hw->rpos = rpos;
 
-        for (sw = hw->sw_head.lh_first; sw; sw = sw->entries.le_next) {
+        for (sw = (SWVoiceOut *)hw->sw_head.lh_first; sw; sw = sw->entries.le_next) {
             if (!sw->active && sw->empty) {
                 continue;
             }
