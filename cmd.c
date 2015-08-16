e4dc688fd8cd02d99291a41931f8cfa4596841e2
SVRajasekhar <surevenkatarajasekhar@gmail.com>
2015-08-16 19:26:39 +0530

    cmd.c
    Pointer Violation
    char **
    breakline(	char	*input,	int	*count);

--- cmd.c
+++ cmd.c
@@ -338,7 +338,7 @@ breakline(
 {
 	int	c = 0;
 	char	*p;
-	char	**rval = calloc(sizeof(char *), 1);
+	char	**rval = (char**)calloc(sizeof(char *), 1);
 
 	while (rval && (p = qemu_strsep(&input, " ")) != NULL) {
 		if (!*p)
@@ -349,8 +349,8 @@ breakline(
 			c = 0;
 			break;
 		}
-		rval[c - 1] = p;
-		rval[c] = NULL;
+		*rval[c - 1] = p;
+		*rval[c] = NULL;
 	}
 	*count = c;
 	return rval;
